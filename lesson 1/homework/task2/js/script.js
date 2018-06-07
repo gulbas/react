85ae50f045cf67332b0390848a3d368fee768860
nbsp <nbsp@nbsp.ru>
2018-06-07 21:06:06 +0300

    add JsDoc finished the task first and second

--- lesson 1/homework/task2/js/script.js
+++ lesson 1/homework/task2/js/script.js
@@ -11,18 +11,52 @@
 
 */
 
-function calculateArea() {
-    let width = arguments[0];
-    let height = arguments[1];
-    let figure = arguments[2];
-    let area = width * height;
-    let obj = {
+/**
+ * Определение площади фигур: квадрат, треугольник, прямоугольник
+ * @param {string} figure - Фигура
+ * @param {...number} param - Параметры
+ */
+function calculateArea(figure, ...param) {
+    let result = document.getElementById('result');
+    let obj;
+    let area;
+    let a = param[0];
+    let b = param[1];
+    let h = param[2];
+    if (figure === 'square') {
+        area = Math.pow(a, a);
+    } else if (figure === 'triangle') {
+        area = 1 / 2 * a * h;
+    } else if (figure === 'rectangle') {
+        area = a * b;
+    }
+
+    obj = {
         area: area,
         figure: figure,
-        input: [width, height]
+        input: []
     };
-    document.write(`${obj.figure} area = ${obj.area}`);
-    console.log(obj);
+    for (let i = 0; i < param.length; i++) obj.input.push(param[i]);
+
+    // Визуализация
+    result.innerHTML = '';
+    result.innerHTML = `${obj.figure} area = ${obj.area}`;
+
+    return console.log(obj);
 }
 
-calculateArea(5, 2, 'square');
+let select = document.getElementById('select');
+let button = document.getElementById('button');
+
+/**
+ * Функция с набором данных в зависимости от фигуры
+ */
+button.addEventListener('click', () => {
+    if (select.value === 'square') {
+        calculateArea('square', 4)
+    } else if (select.value === 'rectangle') {
+        calculateArea('rectangle', 4, 2)
+    } else if (select.value === 'triangle') {
+        calculateArea('triangle', 4, 2, 1)
+    }
+});
